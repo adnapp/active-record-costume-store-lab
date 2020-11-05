28da36e5662cfb64d91dd0378a125c148cd3b570
adnapp <adam.napps@gmail.com>
2020-11-05 13:16:38 -0500

    commit tree 245a8e4b669a4a9e6ac2f5c3ea6883d9db59bec8

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,14 @@
 # Create your costume_stores migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CostumeStores < ActiveRecord::Migration[6.0]
+    def change
+        create_table :costumestores do |t|
+            t.string :name
+
+
+            t.timestamps
+    end
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,14 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class HauntedHouses < ActiveRecord::Migration[6.0]
+    def change
+        create_table :haunedhouses do |t|
+            t.string :name
+
+
+            t.timestamps
+    end
+
+end
